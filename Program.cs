bdea1076b3c986d6f174e18fee6954325197a9f3
Gustavo Roberto Matos dos Santos <gustavo.rmattos@gmail.com>
2021-12-18 13:11:03 -0300

    Teste para verificar or etorno dos dados do BD criado no SQLite

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using LojaProdutos.Models;
+using LojaProdutos.EntityConfig;
+using System.Linq;
 
 namespace LojaProdutos
 {
@@ -6,7 +9,21 @@ namespace LojaProdutos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var produto = new Produto()
+             { Id = 12,
+               Nome = "Garrafa",
+               Preco = 12.4
+             };
+
+            using(var repo = new LojaContext())
+            {
+                var produtos = repo.Produtos.ToList();
+
+                foreach(var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+            }
         }
     }
 }
